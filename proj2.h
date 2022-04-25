7217b6a6449d98e1f662bc8f0abfe012f356c4c9
破壊力 <74363032+re-roll@users.noreply.github.com>
2022-04-25 10:56:08 +0200

    Update proj2.h

--- proj2.h
+++ proj2.h
@@ -1,1 +1,27 @@
+#ifndef _PROJ2_H_
+#define _PROJ2_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <stdarg.h>
+#include <errno.h>
+#include <stdbool.h>
+
+#define ERR_NONE 0 // No errors
+#define ERR_FORM 1 // Invalid format
+#define ERR_MEMO 2 // Semaphore or memory error
+
+#define ERR_NOEX 0 // Handle without exit
+#define ERR_EXIT 1 // Handle and exit
+
+typedef struct argsDef {
+    int NO, NH, TI, TB;
+} argsDef;
+
+argsDef handleArgs(int argc, char *argv[]);
+
+void handleErrs(int ERR, bool ifEXIT, int errNum, char *strErr, ...);
+
+#endif
