d6a932fc615819c1324fc9f1ed5e657df7cfb45a
破壊力 <74363032+re-roll@users.noreply.github.com>
2022-04-27 00:07:01 +0200

    Update proj2.c

--- proj2.c
+++ proj2.c
@@ -1,27 +1,71 @@
 /*
-  ***********************************
+  
   Author: Dmitrii Ivanushkin xivanu00
-  ***********************************
+  
 */
 
 #include "proj2.h"
 
-argsDef args;
-FILE *pf = NULL;
+FILE *pf  = NULL;
+sh_o *mem = NULL;
 
 int main(int argc, char *argv[])
 {
-    errno = 0;
+    int pid;
 
-    args = handleArgs(argc, argv);
+    argsDef args = argsGet(argc, argv);
 
     pf = fopen("proj2.out", "w");
 
+    mem = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED,-1,0);
+    if (mem == MAP_FAILED){
+        fprintf(stderr, "ERROR mapping shared mempry object\n");
+        exit(ERR_ONE);
+    }
+
     if (pf == NULL){
-        handleErrs(ERR_FORM, ERR_EXIT, errno, "ERROR opening file\n");
+        fprintf(stderr, "ERROR opening file\n");
+        exit(ERR_ONE);
+    }
+
+    pid = fork();
+    if (pid == 0) {
+        vodikProces();
+        exit(0);
+    } else if (pid == -1) {
+        fprintf(stderr, "ERROR cannot call fork()");
+        exit(ERR_ONE);
+    } else {
+        pid_t vodikID = pid;
+    }
+
+    pid = fork();
+    if (pid == 0) {
+        hlavniProces();
+        exit(0);
+    } else if (pid == -1) {
+        fprintf(stderr, "ERROR cannot call fork()");
+        exit(ERR_ONE);
+    } else {
+        pid_t hlavniID = pid;
     }
 
     programExit(ERR_NONE);
+}
+
+void hlavniProces()
+{
+
+}
+
+void kyslikProces()
+{
+
+}
+
+void vodikProces()
+{
+
 }
 
 int convToI(char *str)
@@ -31,58 +75,39 @@ int convToI(char *str)
     return val;
 }
 
-argsDef handleArgs(int argc, char *argv[])
+argsDef argsGet(int argc, char *argv[])
 {
-    errno = 0;
-
-    if (argc < 5) {
-        handleErrs(ERR_FORM, ERR_EXIT, errno, "ERROR missing arguments\n");
+    if (argc != 5) {
+        fprintf(stderr, "ERROR missing arguments\n");
+        exit(ERR_ONE);
     }
 
     argsDef args;
-
     args.NO = convToI(argv[1]);
     args.NH = convToI(argv[2]);
     args.TI = convToI(argv[3]);
     args.TB = convToI(argv[4]);
     
     if (args.NO <= 0){
-        handleErrs(ERR_FORM, ERR_EXIT, errno, "ERROR Number of 'O' must be greater than 0\n");
+        fprintf(stderr, "ERROR Number of 'O' must be greater than 0\n");
+        exit(ERR_ONE);
     }
     if (args.NH <= 0){
-        handleErrs(ERR_FORM, ERR_EXIT, errno, "ERROR Number of 'H' must be greater than 0\n");
+        fprintf(stderr, "ERROR Number of 'H' must be greater than 0\n");
+        exit(ERR_ONE);
     }
     if ((args.TI < 0) || (args.TI > 1000)){
-        handleErrs(ERR_FORM, ERR_EXIT, errno, "ERROR Timeout for going to queue must be greater than 0 and lesser than 1000\n");
+        fprintf(stderr, "ERROR Timeout for going to queue must be greater than 0 and lesser than 1000\n");
+        exit(ERR_ONE);
     }
     if ((args.TB < 0) || (args.TB > 1000)){
-        handleErrs(ERR_FORM, ERR_EXIT, errno, "ERROR Timeout for creating molecula must be greater than 0 and lesser than 1000\n");
+        fprintf(stderr, "ERROR Timeout for creating molecula must be greater than 0 and lesser than 1000\n");
+        exit(ERR_ONE);
     }
 
     return args;
 }
-
-void handleErrs(int ERR, bool ifEXIT, int errNum, char *strErr, ...)
-{
-    va_list args;
-    va_start(args, strErr);
-
-    vfprintf(stderr, strErr, args);
-
-    va_end(args);
-    
-    if (errNum != 0)
-        fprintf(stderr, "%s\n", strerror(errNum));
-    
-    if (ifEXIT == true)
-        programExit(ERR);
-}
-
 int programExit(int errType)
 {
-    if ((pf != NULL) && (fclose(pf) == EOF)) {
-        handleErrs(ERR_FORM, 0, errno, "Program cannot close file 'proj2.out'\n");
-    }
-
     exit(errType);
 }
