acf38c1aca6451235334f4d585666831f714ca93
破壊力 <74363032+re-roll@users.noreply.github.com>
2022-05-02 01:40:49 +0200

    Update proj2.h

--- proj2.h
+++ proj2.h
@@ -1,33 +1,42 @@
 #ifndef _PROJ2_H_
 #define _PROJ2_H_
 
+#define _GNU_SOURCE
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
+#include <pthread.h>
 #include <unistd.h>
+#include <string.h>
+#include <limits.h>
 #include <time.h>
 #include <signal.h>
 #include <semaphore.h>
+#include <stdarg.h>
+#include <fcntl.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 #include <sys/shm.h>
+#include <sys/sem.h>
 #include <sys/mman.h>
+#include <sys/types.h>
 
-#define ERR_NONE 0 
-#define ERR_ONE  1 
+#define ERR_NO      0 
+#define ERR_FORMAT  1 
+#define ERR_MEMORY  1 
 
 typedef struct argsDef {
     int NO, NH, TI, TB;
 } argsDef;
 
-typedef struct sh_o{
-    int action, creating, finished;
-}sh_o;
-
 argsDef argsGet(int argc, char *argv[]);
-int programExit(int errType);
+
+void semInit();
 int convToI(char *str);
-void hlavniProces();
-void kyslikProces();
-void vodikProces();
+void ffprintf(FILE *fp1, char const *fmt, ...);
+int programExit(int errType);
+void mainP();
+void oxygenP(int cnt);
+void hydrogenP(int cnt);
 
 #endif
